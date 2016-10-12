364e62522552734f914ce63731f8a4a0840a2f2e
peter.fry <pfry@redhat.com>
2016-10-12 19:06:11 +0100

    transfer code to openshift-ansible-contrib repo

---
+++ vagrant/enterprise/deploy.sh
@@ -0,0 +1,9 @@
+ssh-keygen -b 2048 -t rsa -f /root/.ssh/id_rsa -q -N ""
+for host in admin1.example.com master1.example.com node1.example.com node2.example.com; \
+    do ssh-copy-id -i /root/.ssh/id_rsa.pub $host; \
+    done
+ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/byo/config.yml
+sudo ssh root@master1.example.com htpasswd -b -c /etc/origin/master/htpasswd admin admin123
+
+
+

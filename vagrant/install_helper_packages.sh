317975b378223500ceca0ee52dbd9730f5e76323
Jason DeTiberus <jdetiber@redhat.com>
2016-10-19 17:26:10 -0400

    tweaks
    - remove script files and convert to inline scripts
    - add libvirt support
    - remove static inventory and use ansible_local provisioner config
    - add ORIGIN_OS env var to be able to choose Fedora (untested)
    - change vagrant-cachier scope to machine to avoid locking issues around the
      cache
    - updated sync folder to use the default /vagrant (was causing issues with
      ansible_local provisioner integration)

--- vagrant/install_helper_packages.sh
+++
@@ -1,3 +0,0 @@
-#sudo yum clean all ; yum repolist
-#sudo yum -y update
-sudo yum -y install wget git net-tools bind-utils iptables-services bridge-utils bash-completion rsync
--- vagrant/origin_installer.sh
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/openshift/openshift-ansible
-sudo yum -y install https://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm
-sudo sed -i -e "s/^enabled=1/enabled=0/" /etc/yum.repos.d/epel.repo
-sudo yum -y --enablerepo=epel install ansible pyOpenSSL
-

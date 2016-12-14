d775e11e5b43934bd08c888be8cdc23ba4da22c6
Glenn West <gwest@redhat.com>
2016-12-14 16:12:36 +0800

    Fix comment on ocp product name - Openshift Enterprise to OpenShift Container Platform

--- reference-architecture/azure-ansible/bastion.sh
+++ reference-architecture/azure-ansible/bastion.sh
@@ -259,7 +259,7 @@ cat <<EOF > /home/${AUSERNAME}/postinstall.yml
   tasks:
   - name: Create Master Directory
     file: path=/etc/origin/master state=directory
-  - name: add initial user to OpenShift Enterprise
+  - name: add initial user to Red Hat OpenShift Container Platform
     shell: htpasswd -c -b /etc/origin/master/htpasswd ${AUSERNAME} ${PASSWORD}
 
 EOF

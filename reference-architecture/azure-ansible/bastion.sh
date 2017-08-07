68cef5f5b2f18c5edba4e3947ef0f20a48e672fa
Eduardo Minguez Perez <edu@redhat.com>
2017-08-07 18:09:18 +0200

    Fixed add_host.sh script location
    Otherwise it gets downloaded to the /root folder

--- reference-architecture/azure-ansible/bastion.sh
+++ reference-architecture/azure-ansible/bastion.sh
@@ -487,8 +487,8 @@ oc adm policy add-cluster-role-to-user cluster-admin ${AUSERNAME}
 # Workaround for BZ1469358
 ansible master1 -b -m fetch -a "src=/etc/origin/master/ca.serial.txt dest=/tmp/ca.serial.txt flat=true"
 ansible masters -b -m copy -a "src=/tmp/ca.serial.txt dest=/etc/origin/master/ca.serial.txt mode=644 owner=root"
-curl https://raw.githubusercontent.com/openshift/openshift-ansible-contrib/master/reference-architecture/azure-ansible/add_host.sh -o add_host.sh -s
-chmod a+x ./add_host.sh
+curl https://raw.githubusercontent.com/openshift/openshift-ansible-contrib/master/reference-architecture/azure-ansible/add_host.sh -o /home/${AUSERNAME}/add_host.sh -s
+chmod a+x /home/${AUSERNAME}/add_host.sh
 cat /home/${AUSERNAME}/openshift-install.out | tr -cd [:print:] |  mail -s "${RESOURCEGROUP} Install Complete" ${RHNUSERNAME} || true
 touch /root/.openshiftcomplete
 touch /home/${AUSERNAME}/.openshiftcomplete

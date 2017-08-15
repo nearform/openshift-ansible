2b7486bfcc06e1788b669f0081908dd1e0e4a4e2
Tomas Sedovic <tomas@sedovic.cz>
2017-08-15 09:25:28 +0200

    Pass the extra-vars to install-openshift, too
    Otherwise it won't pick up on our env_id, which means the apps subdomain
    will be incorrect.

--- ci/openstack/install-openshift.sh
+++ ci/openstack/install-openshift.sh
@@ -17,4 +17,4 @@ export INVENTORY="$PWD/playbooks/provisioning/openstack/sample-inventory"
 
 echo INSTALL OPENSHIFT
 
-ansible-playbook --become --timeout 180 --user openshift --private-key ~/.ssh/id_rsa -i "$INVENTORY" ../openshift-ansible/playbooks/byo/config.yml
+ansible-playbook --become --timeout 180 --user openshift --private-key ~/.ssh/id_rsa -i "$INVENTORY" ../openshift-ansible/playbooks/byo/config.yml -e @extra-vars.yaml

7d5613209b2f2875df912b62b21a1618a2b63084
Tomas Sedovic <tomas@sedovic.cz>
2017-09-06 11:38:42 +0200

    Fix openstack CI (#714)
    * Wait for the router and registry pods to come up
    
    The end to end openstack CI started failing intermittently, because
    the openshift-ansible finished without waiting for the router to come
    up. If we waited with the validation, it would succeed.
    
    So this adds two checks that verify that the registry and router are
    actually running or failed before considering the installation done.
    
    * Print diagnostic info about the cluster
    
    oc get nodes, oc status and oc get all can all be useful in
    determining why the test failed.
    
    * Add missing `fi`

--- ci/openstack/install-openshift.sh
+++ ci/openstack/install-openshift.sh
@@ -18,3 +18,41 @@ export INVENTORY="$PWD/playbooks/provisioning/openstack/sample-inventory"
 echo INSTALL OPENSHIFT
 
 ansible-playbook --become --timeout 180 --user openshift --private-key ~/.ssh/id_rsa -i "$INVENTORY" ../openshift-ansible/playbooks/byo/config.yml -e @extra-vars.yaml
+
+
+echo Waiting for the router to come up
+for i in $(seq 15); do
+    if ansible -i "$INVENTORY" masters --user openshift --private-key ~/.ssh/id_rsa -m shell -a 'oc get pod | grep router | grep Running'; then
+        echo Router is running
+        break
+    elif ansible -i "$INVENTORY" masters --user openshift --private-key ~/.ssh/id_rsa -m shell -a 'oc get pod | grep router | grep Failed'; then
+        echo Router failed
+        break
+    else
+        printf .
+        sleep 60
+    fi
+done
+
+echo Waiting for the docker-registry to come up
+for i in $(seq 15); do
+    if ansible -i "$INVENTORY" masters --user openshift --private-key ~/.ssh/id_rsa -m shell -a 'oc get pod | grep docker-registry | grep Running'; then
+        echo Registry is running
+        break
+    elif ansible -i "$INVENTORY" masters --user openshift --private-key ~/.ssh/id_rsa -m shell -a 'oc get pod | grep docker-registry | grep Failed'; then
+        echo Registry failed
+        break
+    else
+        printf .
+        sleep 60
+    fi
+done
+
+echo oc get nodes --show-labels:
+ansible -i "$INVENTORY" masters --user openshift --private-key ~/.ssh/id_rsa -m command -a 'oc get nodes --show-labels'
+
+echo oc status -v:
+ansible -i "$INVENTORY" masters --user openshift --private-key ~/.ssh/id_rsa -m command -a 'oc status -v'
+
+echo oc get all:
+ansible -i "$INVENTORY" masters --user openshift --private-key ~/.ssh/id_rsa -m command -a 'oc get all'

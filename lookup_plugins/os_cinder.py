7eef3fabe0733499de5a55874b17cf8fc30fc8a6
Tomas Sedovic <tomas@sedovic.cz>
2017-10-02 09:49:32 +0200

    Add dynamic inventory
    This adds an `inventory.py` script to the `sample-inventory` that lists
    all the necessary servers and groups dynamically, skipping the
    `static_inventory` role as well as the `hosts` creation.
    
    It also adds an `os_cinder` lookup function which is necessary for a
    seamless Cinder OpenShift registry integration without a static
    inventory.

---
+++ lookup_plugins/os_cinder.py
@@ -0,0 +1,30 @@
+from ansible.errors import AnsibleError, AnsibleParserError
+from ansible.plugins.lookup import LookupBase
+
+import shade
+
+
+class LookupModule(LookupBase):
+
+    def run(self, volume_names, variables=None, **kwargs):
+        cloud = shade.openstack_cloud()
+        volume_attributes = [
+            "id",
+            "name",
+            "display_name",
+            "size",
+            "description",
+        ]
+
+        def get_volume(name_or_id):
+            volume = cloud.get_volume(name_or_id)
+            if not volume:
+                raise AnsibleError(
+                    "Could not find volume: {}".format(name_or_id))
+
+            result = {}
+            for attribute_name in volume_attributes:
+                result[attribute_name] = getattr(volume, attribute_name)
+            return result
+
+        return [get_volume(volume_name) for volume_name in volume_names]
